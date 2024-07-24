f45d6a96b3b84d1af3c5cf503afa60b0316e67bd
LAPTOP <teddy@LAPTOP-FDBLG8FO>
2024-07-24 15:15:01 +1200

    yea

--- easyGui/GuiMadeEasy.h
+++ easyGui/GuiMadeEasy.h
@@ -27,6 +27,7 @@ private:
 	std::vector<SDL_Color> ColorList;
 	std::vector<SDL_Texture*> TextureList;
 	std::vector<bool> VisibleList;
+	std::vector<bool> InteractableList;
 
 	std::vector<genericData*> Descendants;
 
@@ -37,7 +38,50 @@ public:
 
 	}
 
-	int createFrame(int x, int y, int w, int h, int visible, SDL_Color color) {
+	int processClick()
+
+	int createButton(int x, int y, int w, int h, bool visible, bool interactable, SDL_Color color) {
+		// create a genericdata and assign variables to the correct places
+		genericData* newButton = new genericData;
+		newButton->type = Button;
+
+		IntVector sizeToAssign = { w,h };
+		IntVector positionToAssign = { x,y };
+
+		newButton->dataPositions.resize(5);
+
+		newButton->dataPositions[0] = VisibleList.size();
+		VisibleList.push_back(visible);
+
+		newButton->dataPositions[1] = InteractableList.size();
+		InteractableList.push_back(interactable);
+
+		newButton->dataPositions[2] = SizeList.size();
+		SizeList.push_back(sizeToAssign);
+
+		newButton->dataPositions[3] = PositionList.size();
+		PositionList.push_back(positionToAssign);
+
+		newButton->dataPositions[4] = ColorList.size();
+		ColorList.push_back(color);
+
+		//create the texture for the button
+		SDL_Surface* surface = SDL_CreateRGBSurfaceWithFormat(0, w, h, 32, SDL_PIXELFORMAT_RGBA32);
+
+		Uint32 nColor = SDL_MapRGBA(surface->format, color.r, color.g, color.b, color.a);
+		SDL_FillRect(surface, NULL, nColor);
+
+		SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+		newButton->dataPositions[5] = TextureList.size();
+		TextureList.push_back(texture);
+
+		SDL_FreeSurface(surface);
+
+		return 0;
+	}
+
+	int createFrame(int x, int y, int w, int h, bool visible, SDL_Color color) {
 		// create a genericdata and assign variables to the correct places
 		genericData* newFrame = new genericData;
 		newFrame->type = Frame;
@@ -71,5 +115,7 @@ public:
 		TextureList.push_back(texture);
 
 		SDL_FreeSurface(surface);
+
+		return 0;
 	}
 };
